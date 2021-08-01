927bd6665b0b64857be0cc0477b8b1070e3144ec
Davit Mamasakhlisi <46651578+Mamasakhlisi@users.noreply.github.com>
2021-08-01 07:57:27 -0700

    Update alias name

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
     ],
     resolve: {
         alias: {
-            Utilities: path.resolve(__dirname, './src/components'),
+            components: path.resolve(__dirname, './src/components'),
         }
     },
     module: {
